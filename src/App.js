994ef5014bf81ef264b2373bbb5acde34dc51060
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-08-05 21:43:51 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+function ChildIframe({ targetButton }) {
+  const handleIframeMessage = (event) => {
+    // Ensure the message is from the correct origin (the parent domain)
+    if (event.origin === 'http://localhost:3000') {
+      // Here, event.data will contain the message sent from the parent
+      console.log('Received message from parent:', event.data);
+      // Implement your logic to process the message from the parent (if needed).
+    }
+  };
+
+  const handleButtonClick = () => {
+    // Send a message to the parent with the button identifier
+    window.parent.postMessage(targetButton, 'http://localhost:3000');
+
+    // Add a trigger to search for "Go to Google" on the parent page
+    window.parent.searchFunction('Go to Google');
+  };
+
+  React.useEffect(() => {
+    // Add an event listener to listen for messages from the parent
+    window.addEventListener('message', handleIframeMessage);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener('message', handleIframeMessage);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Your iframe content */}
+      <h2>Child Webpage Content Acts as Chatbot</h2>
+      <button onClick={handleButtonClick}>Search for Button</button>
     </div>
   );
 }
 
-export default App;
+export default ChildIframe;
